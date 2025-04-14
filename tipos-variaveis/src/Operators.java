a60cbd8ea45da9cd8f0865f9aa0384c37e268dbb
joaocase <jcasamelofinanceiro@gmail.com>
2025-04-14 18:29:23 -0300

    modifying the project with some examples of operators

---
+++ tipos-variaveis/src/Operators.java
@@ -0,0 +1,25 @@
+public class Operators {
+    public static void main(String[] args) {
+        String concatenation = "?";
+
+        concatenation = 1+1+1+"1";
+
+        System.out.println(concatenation);
+
+        concatenation = 1+"1"+1+1;
+
+        System.out.println(concatenation);
+
+        concatenation = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenation);
+
+        concatenation = "1"+1+"1"+1;
+
+        System.out.println(concatenation);
+
+        concatenation = "1" + (1+1+1);
+
+        System.out.println(concatenation);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome, DIO class");
+        //System.out.println("Welcome, DIO class");
+
+        /*short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroNormal;
+
+        System.out.println(numeroCurto2);*/
+
+        int sum = 20 + 50;
+        double result = sum + (sum * (5.0/100));
+        System.out.printf("Resultado: R$%.2f", result);
     }
 }
